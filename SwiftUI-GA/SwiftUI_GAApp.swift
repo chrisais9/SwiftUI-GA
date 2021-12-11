9b93ae2301c1f956134af95bc24b1358f65ae0b4
chrisais9 <chrisais9@naver.com>
2021-12-12 01:02:04 +0900

    Initial Commit

---
+++ SwiftUI-GA/SwiftUI_GAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GAApp.swift
+//  SwiftUI-GA
+//
+//  Created by 구형모 on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
