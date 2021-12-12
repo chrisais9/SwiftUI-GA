437607b63072b32d5260cc7c93f2883b7411a14b
chrisais9 <chrisais9@naver.com>
2021-12-13 01:06:56 +0900

    make main view to mark touched coordinate with circle

--- SwiftUI-GA/ContentView.swift
+++ SwiftUI-GA/ContentView.swift
@@ -8,10 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var xPos: CGFloat = 0
+    @State var yPos: CGFloat = 0
+    
+    @State var locations: [CGPoint] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { _ in
+            ForEach(0..<locations.count, id:\.self) { index in
+                CirclesView(index: index, offset: CGSize(width: locations[index].x, height: locations[index].y))
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.thickMaterial)
+        .gesture(DragGesture(minimumDistance: 0).onEnded { dragGesture in
+            locations.append(dragGesture.startLocation)
+            self.xPos = dragGesture.location.x
+            self.yPos = dragGesture.location.y
+        })
     }
+}
+
+struct CirclesView: View {
+    
+    let index: Int
+    let offset: CGSize
+    
+    var body: some View {
+        
+        Circle()
+            .strokeBorder(Color.blue, lineWidth: 2.0, antialiased: true)
+            .frame(width: 50, height: 50, alignment: .center)
+            .overlay(Text(String(describing: index)))
+            .offset(offset)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
